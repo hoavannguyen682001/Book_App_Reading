210aca9ca1d8dd1b8fc1f87a615bc320bab6b16b
Tran Khanh <khalongtvh@gmail.com>
2021-11-02 15:00:32 +0700

    commit sửa sign up activity

--- app/src/main/java/com/nguyenvanhoa/book_app_reading/Activity/SignUpActivity.java
+++ app/src/main/java/com/nguyenvanhoa/book_app_reading/Activity/SignUpActivity.java
@@ -80,13 +80,15 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     private void InitSpinner() {
-        ArrayList<String> listGender = new ArrayList<>();
-        listGender.add("Male");
-        listGender.add("Female");
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listGender);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spnGender.setAdapter(adapter);
+//        ArrayList<String> listGender = new ArrayList<>();
+//        listGender.add("Male");
+//        listGender.add("Female");
+//        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this, android.R.layout., listGender);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spnGender.setAdapter(adapter);
+        String[] arrGender = new String[]{"Male", "Female"};
+        ArrayAdapter<CharSequence> GenderAdapter = new ArrayAdapter<CharSequence>(this, R.layout.custom_spinner, arrGender);
+        spnGender.setAdapter(GenderAdapter);
     }
 
 }
