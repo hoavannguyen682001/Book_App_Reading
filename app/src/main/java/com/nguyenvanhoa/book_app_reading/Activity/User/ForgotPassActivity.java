7a583d5f8d2ebe79d8f542c2ea1a31ec98c76899
hoava <hoavannguyen682001@gmail.com>
2021-12-14 14:44:33 +0700

    Update feature Forgot password

--- app/src/main/java/com/nguyenvanhoa/book_app_reading/Activity/User/ForgotPassActivity.java
+++ app/src/main/java/com/nguyenvanhoa/book_app_reading/Activity/User/ForgotPassActivity.java
@@ -1,38 +1,56 @@
 package com.nguyenvanhoa.book_app_reading.Activity.User;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.nguyenvanhoa.book_app_reading.R;
+import com.nguyenvanhoa.book_app_reading.databinding.ActivityForgotPassBinding;
 
 public class ForgotPassActivity extends Activity {
+    private ActivityForgotPassBinding binding;
 
-    private TextView tvExit, tvSignUp;
-    private Button btnSendCode;
+    //firebase auth
+    private FirebaseAuth firebaseAuth;
+
+    //progress dialog
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forgot_pass);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        tvExit = findViewById(R.id.tvExit);
-        tvSignUp = findViewById(R.id.tvSignUp);
-        btnSendCode = findViewById(R.id.btnSendCode);
 
-        tvExit.setOnClickListener(new View.OnClickListener() {
+        binding = ActivityForgotPassBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //init/setup progress dialog
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please wait");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        binding.tvExit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent( getApplication(), LoginActivity.class);
-                startActivity(intent);
+                onBackPressed();
             }
         });
-        tvSignUp.setOnClickListener(new View.OnClickListener() {
+        binding.tvSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent( getApplication(), SignUpActivity.class);
@@ -40,11 +58,49 @@ public class ForgotPassActivity extends Activity {
             }
         });
 
-        btnSendCode.setOnClickListener(new View.OnClickListener() {
+        binding.btnSendCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplication(), "Check Your Email", Toast.LENGTH_SHORT).show();
+                validateData();
             }
         });
+    }
+    private String email = "";
+    private void validateData() {
+        //get data i.e. email
+        email = binding.txtEmail.getText().toString().trim ();
+        //validate data e.g. shouldn't empty and should be valid format
+        if (email.isEmpty()) {
+            Toast.makeText(this, "Enter Email...", Toast.LENGTH_SHORT).show();
+        }
+        else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            Toast.makeText(this, "Invalid email format...", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            recoverPassword();
+        }
+    }
+    private void recoverPassword() {
+        //show progress
+        progressDialog.setMessage("Sending password recovery instructions to " + email);
+        progressDialog.show();
+        //begin sending recovery
+        firebaseAuth.sendPasswordResetEmail(email)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        //sent
+                        progressDialog.dismiss();
+                        Toast.makeText(ForgotPassActivity.this, "Instructions to reset password sent to " + email, Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //failed to send
+                        progressDialog.dismiss();
+                        Toast.makeText(ForgotPassActivity.this, "Failed to send due to " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
